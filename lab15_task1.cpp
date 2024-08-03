a57fdfdbfe387e47e4ddcb84bfa8bbcbd903d93f
Darya Kuznetsova <Dashar1996@gmail.com>
2024-08-03 21:48:28 +0300

    Add files via upload

---
+++ lab15_task1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+struct StudentGrade {
+    string name;
+    char grade;
+};
+
+int main() {
+    system("chcp 1251");
+    map<string, char> studentGrades;
+
+    studentGrades["Ben"] = 'A';
+    studentGrades["Sofy"] = 'B';
+    studentGrades["Tom"] = 'C';
+    studentGrades["Ron"] = 'D';
+
+   
+    for (const auto& student : studentGrades) {
+        cout << student.first << " " << student.second << endl;
+    }
+
+    studentGrades["Tom"] = 'B';
+
+    cout << "\nОбновленные оценок " << endl;
+    for (const auto& student : studentGrades) {
+        cout << student.first << " " << student.second << endl;
+    }
+
+    return 0;
+}

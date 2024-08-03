b4531d54073ea783983c630bbcb917ebbbd7aec1
Darya Kuznetsova <Dashar1996@gmail.com>
2024-08-03 15:32:37 +0300

    Add files via upload

---
+++ lab11_task2/Point.cpp
@@ -0,0 +1,18 @@
+﻿#include "Point.h"
+#include <cmath>
+using namespace std;
+
+Point::Point(double x, double y) : x(x), y(y) {}
+
+double Point::distance() const {
+    return sqrt(x * x + y * y);
+}
+
+ostream& operator<<(ostream& os, const Point& point) {
+    os <<  point.x << ", " << point.y ;
+    return os;
+}
+
+bool Point::operator<(const Point& other) const {
+    return distance() < other.distance();
+}
